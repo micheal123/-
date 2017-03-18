4327b14932047b72f22e4c4c0526f289355b86cd
wyh <294634047@qq.com>
2017-03-18 22:10:14 +0800

    update

--- ph_main.cpp
+++ ph_main.cpp
@@ -14,94 +14,110 @@ class member{
 #include"ph_sq.h"
 #include"ph_link.h"
 using namespace std;
-void print(){
-	printf("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\n");  
-   		printf("*=*               程序说明                *=*\n");  
-   	    printf("*=*    请及时保存创建完毕的通讯录内容!    *=*\n");  
-        printf("*=*               C语言通讯录             *=*\n");  
-        printf("*=*               1.创建新的通讯录        *=*\n");
-        printf("*=*               2.保存通讯录            *=*\n");
-        printf("*=*               3.查看通讯录            *=*\n");
-        printf("*=*               4.添加用户              *=*\n");
-        printf("*=*               5.删除用户              *=*\n");
-        printf("*=*               6.修改用户              *=*\n");
-        printf("*=*               7.查看被删目录          *=*\n");
-        printf("*=*               8.查找用户              *=*\n");
-        printf("*=*               9.退出系统              *=*\n");
-        printf("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\n");
-          
-        printf("请输入1-9的功能编号 ：\n");  
-	
-}	
 int main(){
-  	ElemeType t;
+  	ElemeType t,num;
   	class member *m ,*pm,*d,*pd,*e;//m是通讯录的原文件pm为第一个元素，d是被删除的表（链表）
 //	  d=new member();//新建一个头节点
 //	  pd=d;  
-	seq sq;//实例化一个类	
-	freopen("data.txt","r",stdin);
+		seq sq;//实例化一个类	
+		freopen("data.txt","r",stdin);
 		scanf("%d",&t);
+		if(t==NULL){
+		t=0;
+		}
 		sq.setseq(t,MAXSIZE); 
 		sq.InitList(m);//初始化 	
 	  	pm=m;//将头指针存放 
 		sq.ReadFile(m);	
 		fclose(stdin);
-		freopen("CON", "r ", stdin); 		
-		int num;	
-	
-while(1){	
-print();
-	scanf("%d",&num); 
+		freopen("CON", "r ", stdin); 
+					print();		
+	while(1){	
+			scanf("%d",&num); 
 		 switch(num)  
         {  
         case 1:  
             {  
-          
+            sq.setseq(0,MAXSIZE); 
+            sq.InitList(m);
+            	pm=m;
 				break;   
             }  
             
         case 2:  
             {  
+			printf("正在保存中....\n");
+             freopen("data.txt","w",stdout);
+             m=pm;
+			 sq.SaveFile(m);
+			 fclose(stdout);
+			freopen("CON","w",stdout);	
+			 printf("保存成功！\n");
                 /*head=paixu(head); */  
 				break;                              //保存通讯录  
             }  
             
         case 3:  
             { 
-				//从文件中读入 
-		m=pm;
-		sq.QueryList(m);//
+			m=pm;
+			sq.QueryList(m);//
                 /*head=sq.DeletList(m,2);*/                               //查看通讯录   
            break;  
 		     }  
            
         case 4:
-		 {                                               //添加用户  
+		 {     
+		 while(1){                                          //添加用户  
 		 		e=new member();
-		 		e->id=4;//插入第几项又用户指定 
-				strcpy(e->name,"wwwww");
-				strcpy(e->sex,"男");
-				strcpy(e->remark,"wwww");
-		 		e->tle1=12321321;
-				e->tle2=1111111;
-		 		sq.InsertList(m,e); 
-				m=pm;
-				sq.QueryList(m); 
+		 		cout<<"请输入添加的位置：\n";
+			 	scanf("%d",&e->id);
+				cout<<"请依次输入姓名，性别，电话1，电话2，备注信息：\n"; 
+		 		cin>>e->name>>e->sex>>e->tle1>>e->tle2>>e->remark;
+//		 	e->id=2;//插入第几项又用户指定 
+//				strcpy(e->name,"wwwww");
+//				strcpy(e->sex,"男");
+//				strcpy(e->remark,"wwww");
+//		 		e->tle1=12321321;
+//				e->tle2=1111111;
+				 	if(sq.InsertList(m,e))
+			{	
+				printf("插入成功\n"); 
+				break;
+			}else{
+				printf("插入失败，请重新输入：\n"); 
+			}
+				 
+		}
 				break;  
             }  
             
         case 5:  
             {  
-               	
-		           sq.DeletList(m,2);
-	             	m=pm;
-		           sq.QueryList(m);  
+            int id;	
+			printf("请输入要删除的序号：\n");
+			while(1){  
+            scanf("%d",&id);
+            if(id==-1)
+				break;
+		    if(sq.DeletList(m,id)){
+		           	printf("删除成功！！！\n");
+		           	break;
+			}else{
+					printf("删除失败！！！输入序号有误，请重新输入或者输入-1退回主菜单\n");
+				}	
+			}  
 				       break;                          //删除用户  
             }  
            
         case 6:  
             {  
-                /*head=load(head);*/   
+			while(1){
+            	  printf("请输入要修改的条数：\n") ;
+            	 if(sq.Update(m)){
+            	 	printf("修改成功！\n"); 
+					 break;
+				 }
+			}
 				break;                            //修改用户   
             }  
             
@@ -124,12 +140,13 @@ print();
 				break; 
             }  
            
-        default:  
-            printf("操作错误，此项不存在!\n");  
-        }  
+        default:  {
+        	printf("操作错误，此项不存在!\n"); 
+			break;
+		}
+    }  
 }
 	
-//  	printf("%s",pm->name);
 	return 0;
 }
  
--- ph_sq.h
+++ ph_sq.h
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std; 
 class seq{
 	private:
 		int length;//当前长度 
@@ -6,9 +8,11 @@ class seq{
 		void setseq(int l,int s );
 		Status InitList(member* &m); 
 		void ReadFile(member* &m);
-		void QueryList(member *m);
+		void QueryList(member *m);	
+		void SaveFile(member *m);
 		Status DeletList(member* &m,ElemeType index);
 		Status InsertList(member* &m,member *e);
+		Status Update(member *m);
 }; 
 Status seq::InitList(member* &m){
 	m=new member[size];
@@ -20,6 +24,7 @@ Status seq::InitList(member* &m){
 //Status DestoryList(member &m){
 //	
 //}
+
 void seq::ReadFile(member* &m){
 	int temp;
 	 temp=length;  
@@ -36,13 +41,19 @@ void seq::setseq(int l,int s){
 		size=s;
 }
 void seq::QueryList(member *m){
+	member *t;
 	int temp;
+	t=m;
 	temp=length;
 	while(temp){
-		printf("%d\t%s\t%s\t%d\t%d\t%s\n",(m->id),(m->name),(m->sex),(m->tle1),(m->tle2),(m->remark));
+		printf("%d\t%s\t%s\t%d\t%d\t%s\n",(t->id),(t->name),(t->sex),(t->tle1),(t->tle2),(t->remark));
 		temp--;
-		m++;
+		t++;
 	}
+}
+void seq::SaveFile(member *m){
+	printf("%d\n",length);
+	QueryList(m);
 }
 Status seq::DeletList(member* &m,ElemeType index){
 	member *end;
@@ -60,6 +71,49 @@ Status seq::DeletList(member* &m,ElemeType index){
 	return OK;
 	
 }
+	Status seq::Update(member *m){
+		 	member *temp;
+			 int id,choose;
+                scanf("%d",&id);
+                if(id==-1)
+                	return OK;
+				temp=m+id-1;
+                 printf("请选择要修改的内容：\n1.姓名\t2.性别\t3.电话1\t4.电话2\t5.备注");
+                 scanf("%d",&choose);
+                 switch(choose){
+                 	case 1:
+                 		{
+                 		cin>>(temp->name); 
+                 			break;	
+						 }
+                 	case 2:
+                 		{
+                 				cin>>(temp->sex); 
+                 			break;	
+						 }
+                 	case 3:
+                 		{
+                 				cin>>(temp->tle1); 
+                 			break;	
+						 }
+				 	case 4:
+                 		{
+                 				cin>>(temp->tle2); 
+                 			break;	
+						 }
+					case 5:
+                 		{
+                 			cin>>(temp->remark); 
+                 			break;	
+						 }
+					default:
+						{
+							printf("选择错误请重新输入,或者输入-1退回主菜单\n");
+							return ERROR; 
+						}
+				 }
+				 return OK;
+	}
 
 Status seq::InsertList(member* &m,member *e){
 		int id,temp;
@@ -82,4 +136,22 @@ Status seq::InsertList(member* &m,member *e){
 		return OK;
 	
 }
-
+void print(){
+	printf("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\n");  
+   		printf("*=*               程序说明                *=*\n");  
+   	    printf("*=*    请及时保存创建完毕的通讯录内容!    *=*\n");  
+        printf("*=*               手机通讯录             *=*\n");  
+        printf("*=*               1.创建新的通讯录        *=*\n");
+        printf("*=*               2.保存通讯录            *=*\n");
+        printf("*=*               3.查看通讯录            *=*\n");
+        printf("*=*               4.添加用户              *=*\n");
+        printf("*=*               5.删除用户              *=*\n");
+        printf("*=*               6.修改用户              *=*\n");
+        printf("*=*               7.查看被删目录          *=*\n");
+        printf("*=*               8.查找用户              *=*\n");
+        printf("*=*               9.退出系统              *=*\n");
+        printf("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\n");
+          
+        printf("请输入1-9的功能编号 ：\n");  
+	
+}
