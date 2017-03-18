2a70b9341a7acbe948ea8940a97328ce57f10162
wyh <294634047@qq.com>
2017-03-18 13:06:55 +0800

    update

--- ph_main.cpp
+++ ph_main.cpp
@@ -1,4 +1,5 @@
 #include<cstdio>
+#include<string.h>
 #define ERROR -1
 #define OK 1
 #define MAXSIZE 80
@@ -12,18 +13,45 @@ class member{
 };
 #include"ph_sq.h"
 #include"ph_link.h"
+using namespace std;
 int main(){
   	ElemeType t;
-  	struct member *m ,*pm,*d,*pd;//m是通讯录的原文件pm为第一个元素，d是被删除的表（链表）
+  	class member *m ,*pm,*d,*pd,*e;//m是通讯录的原文件pm为第一个元素，d是被删除的表（链表）
 //	  d=new member();//新建一个头节点
 //	  pd=d;  
-		InitList(m);
-	  	pm=m;
-		freopen("data.txt","r",stdin);
-		ReadFile(t,m);
+	seq sq;//实例化一个类	
+	freopen("data.txt","r",stdin);
+		scanf("%d",&t);
+		sq.setseq(t,MAXSIZE); 
+		sq.InitList(m);//初始化 
+	  	pm=m;//将头指针存放 
+	  	//从文件中读入 
+		sq.ReadFile(m);
 		m=pm;
-		QueryList(t,m);
-			fclose(stdin);	
+		sq.QueryList(m);
+		fclose(stdin);
+			//删除
+		printf("\n");		
+		sq.DeletList(m,2);
+		m=pm;
+		sq.QueryList(m);
+		//添加
+			printf("\n");
+		 e=new member();
+		 e->id=4;//插入第几项又用户指定 
+		strcpy(e->name,"wwwww");
+		strcpy(e->sex,"男");
+		strcpy(e->remark,"wwww");
+		 e->tle1=12321321;
+		 e->tle2=1111111;
+		 sq.InsertList(m,e); 
+		m=pm;
+		sq.QueryList(m);
+		//修改 
+		 
+		
+		
+	
 		
 	  
 //  	printf("%s",pm->name);
--- ph_sq.h
+++ ph_sq.h
@@ -1,12 +1,17 @@
-//class seq{
-//	private:
-//		int length;
-//		int size;
-//	public:
-//		void setseq(int length,int )
-//}; 
-Status InitList(member* &m){
-	m=new member[MAXSIZE];
+class seq{
+	private:
+		int length;//当前长度 
+		int size;//总共大小 
+	public:
+		void setseq(int l,int s );
+		Status InitList(member* &m); 
+		void ReadFile(member* &m);
+		void QueryList(member *m);
+		Status DeletList(member* &m,ElemeType index);
+		Status InsertList(member* &m,member *e);
+}; 
+Status seq::InitList(member* &m){
+	m=new member[size];
 	if(!m)
 	return ERROR;
 	else
@@ -15,10 +20,9 @@ Status InitList(member* &m){
 //Status DestoryList(member &m){
 //	
 //}
-void ReadFile(ElemeType &t,member* &m){
+void seq::ReadFile(member* &m){
 	int temp;
-	 scanf("%d",&t);
-	 temp=t;  
+	 temp=length;  
 	  while(temp)
 	   {	
 	   	scanf("%d%s%s%d%d%s",&(m->id),&(m->name),&(m->sex),&(m->tle1),&(m->tle2),&(m->remark));
@@ -27,14 +31,55 @@ void ReadFile(ElemeType &t,member* &m){
 	   }
 	
 }
-void QueryList(ElemeType t,member *m){
-	while(t){
+void seq::setseq(int l,int s){
+		length=l;
+		size=s;
+}
+void seq::QueryList(member *m){
+	int temp;
+	temp=length;
+	while(temp){
 		printf("%d\t%s\t%s\t%d\t%d\t%s\n",(m->id),(m->name),(m->sex),(m->tle1),(m->tle2),(m->remark));
-		t--;
+		temp--;
 		m++;
 	}
 }
-Status ListInsert(member &m,ElemeType i,member e){
+Status seq::DeletList(member* &m,ElemeType index){
+	member *end;
+	end=m+length-1;//定义表尾 
+	if(index>length||index<1)
+		return ERROR;
+	m=m+index-1;
+	while(m<=end){
+		*m=*(m+1);//删掉一个，往前挪一位 
+		m->id=index;
+		m++;
+		index++;
+	}
+	length--;
+	return OK;
+	
+}
+
+Status seq::InsertList(member* &m,member *e){
+		int id,temp;
+		member *end;
+		end=m+length-1;//定义表尾 
+		id=e->id;
+		if(id>(length+1)||id<0)
+			return ERROR;	//序号错误 直接返回 
+		m=m+id-1;//定义要插入的位置 
+		temp=length+1;
+		printf("%d\n",temp);
+		while(end>=m){
+			*(end+1)=*end;//将都往后移动一位 
+			(end+1)->id=temp;//将移动后的元素序号改为1 
+			end--; 
+			temp--;	
+		}
+		length++;
+		*(end+1)=*e;//插入 
+		return OK;
 	
 }
 
